5b6498fce613f5d2f9db16f8b051baa1f2fb44d0
Fuki-san <ff1028mh@gmail.com>
2023-10-26 09:59:55 +0900

    【修正】Taxi.php-if文の分岐処理簡略化

--- oop_2/Taxi.php
+++ oop_2/Taxi.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/Car.php';
 class Taxi extends Car
 {
-    
+
     public function __construct($name, $number, $color)
     {
         parent::__construct($name, $number, $color);
@@ -10,33 +10,26 @@ class Taxi extends Car
 
     public function pickUp($geton_nm)
     {
-        $on_number = '';
-        if ($geton_nm == 1) {
-            $on_number = "{$geton_nm}人乗車しました" . PHP_EOL;
+        $on_result = '';
+        if ($geton_nm == 1 || $geton_nm == 2) {
+            $on_result = "{$geton_nm}人乗車しました" . PHP_EOL;
             $this->passenger += $geton_nm;
-            
-        } elseif ($geton_nm == 2) {
-            $on_number = "{$geton_nm}人乗車しました" . PHP_EOL;
-            $this->passenger += $geton_nm;
-        } 
-        return $on_number;
+        }
+        return $on_result;
     }
 
     public function lower($getoff_nm)
     {
-        $off_number = '';
-        if ($this->passenger > 0 && $getoff_nm == 1) {
-            $off_number = "1人降車しました" . PHP_EOL;
+        $off_result = '';
+        if ($this->passenger - $getoff_nm > 0) {
+            $off_result = "{$getoff_nm}人降車しました" . PHP_EOL;
             $this->passenger -= $getoff_nm;
-        }elseif ($this->passenger < 2 && $getoff_nm == 2) {
-            $off_number = "2人は降車できません" . PHP_EOL;
-        } elseif ($getoff_nm == 2) {
-            $off_number = "2人降車しました" . PHP_EOL;
-            $this->passenger -= $getoff_nm;
-        } 
+        } elseif ($this->passenger - $getoff_nm < 0) {
+            $off_result = "{$getoff_nm}人は降車できません" . PHP_EOL;
+        }
+
         //1つ目のelseifのが2つ目(最後)のelseifより上！理由は、1つ目は2つ目に包括されているから、
         //それでも違う処理を出力させたい場合は優先順位的に上に記載することでうまく処理されるから
-        return $off_number;
+        return $off_result;
     }
 }
- 
